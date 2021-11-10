801c10410d0579ba113f76c943aa444b4f8d5fda
Gian Carlos <gpaucarcortez@gmail.com>
2021-11-09 20:55:07 -0500

    Header App

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,41 +1,69 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
-
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
-  initState(){
+  initState() {
     super.initState();
     getData();
   }
 
-  getData()async{
+  getData() async {
     String path = "http://192.168.1.5:8000/api/task-list/";
     Uri _uri = Uri.parse(path);
     http.Response response = await http.get(_uri);
     print(response.body);
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-
-          ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Bienvenido",
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                      Text(
+                        "My Tasks",
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 40.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                  CircleAvatar(
+                    radius: 26,
+                    backgroundImage: NetworkImage("https://images.pexels.com/photos/412840/pexels-photo-412840.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
-
     );
   }
 }
